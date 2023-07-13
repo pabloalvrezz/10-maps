1580e2e748aceb389f207a7a0d335182985b8228
Pablo <pabloag43@educastur.es>
2023-07-13 09:44:19 +0200

    Añadimos  la opcion de poder añadir marcadores

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,14 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+import { LngLat, Map, Marker } from 'mapbox-gl';
+import { elementAt } from 'rxjs';
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css']
 })
-export class MarkersPageComponent implements OnInit {
+export class MarkersPageComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('map')
+  public divMap?: ElementRef;
 
-  ngOnInit(): void {
+  public map?: Map;
+  public actualCenter: LngLat = new LngLat(-74.5, 40); // propiedad que usaremos para controlar el centro del mapa
+  public userMarkers: Marker[] = [] // array en el que almacenaremos los marcadores del usuario
+
+
+  ngAfterViewInit(): void {
+
+    // validaremos que siempre exista divMap
+    if (!this.divMap) throw 'El elemento del mapa no ha sido encontrado';
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.actualCenter, // starting position [lng, lat]
+      zoom: 10, // starting zoom
+    });
+
+    //   const markerHtml = document.createElement('div')
+    //   markerHtml.innerHTML = 'User´s marker'
+
+    //   const marker = new Marker(
+    //     {
+    //       color: '#252525',
+    //       element: markerHtml
+    //     }
+    //   )
+    //     .setLngLat(this.actualCenter)
+    //     .addTo(this.map)
   }
 
+  createMarker() {
+    if (!this.map) return;
+
+    const color = '#xxxxxx'.replace(/x/g, y => (Math.random() * 16 | 0).toString(16)); // variable para generar colores aleatorios
+    const lngLat = this.map?.getCenter()
+
+    this.addMarker(lngLat, color)
+  }
+
+  // metodo que usaremos para añadir un marcador al elemento
+  addMarker(lngLat: LngLat, color: string) {
+    if (!this.map) return;
+
+    const marker = new Marker(
+      {
+        color,
+        draggable: true,
+      }
+    ).setLngLat(lngLat).addTo(this.map)
+
+    this.userMarkers.push(marker);
+  }
 }
--- src/app/maps/pages/zoom-page/zoom-page.component.ts
+++ src/app/maps/pages/zoom-page/zoom-page.component.ts
@@ -1,28 +1,76 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 
-import { Map } from 'mapbox-gl';
+import { LngLat, Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-page.component.html',
   styleUrls: ['./zoom-page.component.css']
 })
-export class ZoomPageComponent implements AfterViewInit {
+export class ZoomPageComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('map')
   public divMap?: ElementRef;
+
+  public actualZoom: number = 10; // propiedad que usaremos para controlar el zoom del mapa
+  public map?: Map;
+  public actualCenter: LngLat = new LngLat(-74.5, 40); // propiedad que usaremos para controlar el centro del mapa
 
   ngAfterViewInit(): void {
 
     // validaremos que siempre exista divMap
     if (!this.divMap) throw 'El elemento del mapa no ha sido encontrado';
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      center: this.actualCenter, // starting position [lng, lat]
+      zoom: this.actualZoom, // starting zoom
     });
+
+    this.mapListeners()
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove
+  }
+
+  // evento que usaremos para escuchar los eventos del mapa
+  mapListeners() {
+    // debemos comprobar que siempre exista el mapa
+    if (!this.map) throw 'El mapa no ha sido actualizado'
+
+    // controlamos el zoom que tiene el mapa
+    this.map.on('zoom', () => {
+      this.actualZoom = this.map!.getZoom();
+    });
+
+    // controlamos el zoom que estamos haciendo y no dejamos que sobrepase el nivel maximo
+    this.map.on('zoomend', () => {
+      if (this.map!.getZoom() < 18) return;
+
+      this.map!.zoomTo(18)
+    });
+
+    // controlamos las coordenadas del mapa
+    this.map.on('move', () => {
+      this.actualCenter = this.map!.getCenter();
+    })
 
   }
 
+  // metodo que usaremos para controlar el valor que ha cambiado del zoom
+  zoomChanged(value: string) {
+    this.actualZoom = Number(value);
+    this.map?.zoomTo(this.actualZoom);
+  }
+
+  // metodo que usaremos para controlar el zoom in
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  // metodo que usaremos para controlar el zoom out
+  zoomOut() {
+    this.map?.zoomOut();
+  }
 }
